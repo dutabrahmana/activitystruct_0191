671838405da93fcf4a31c5dc324a22bd198bdea7
dutabrahmana <dutalinggau25@gmail.com>
2024-04-02 10:09:22 +0700

    membuat isi dri for trsebut

--- Praktikum Part4/Praktikum Part4.cpp
+++ Praktikum Part4/Praktikum Part4.cpp
@@ -14,4 +14,8 @@ struct mahasiswa {
 int main() {
 	mahasiswa mhs[3];
 	for (int i = 0; i < 3; i++) {
-		co
+		cout << "data ke-" << (i + 1) << ":" << endl;
+		cout << "Nomor mahasiswa : ";
+		getline (cin, mhs[i].nim);
+		cout << "Nama Mahasiswa : ";
+		getline(cin, mhs[i].nama);
