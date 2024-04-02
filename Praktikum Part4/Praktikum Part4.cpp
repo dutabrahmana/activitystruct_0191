ef09cc19235c2f3b1a11ac8887a16bb167a36bf8
dutabrahmana <dutalinggau25@gmail.com>
2024-04-02 10:10:34 +0700

    membuat cout cin untuk memnagil data

--- Praktikum Part4/Praktikum Part4.cpp
+++ Praktikum Part4/Praktikum Part4.cpp
@@ -16,6 +16,18 @@ int main() {
 	for (int i = 0; i < 3; i++) {
 		cout << "data ke-" << (i + 1) << ":" << endl;
 		cout << "Nomor mahasiswa : ";
-		getline (cin, mhs[i].nim);
+		getline(cin, mhs[i].nim);
 		cout << "Nama Mahasiswa : ";
 		getline(cin, mhs[i].nama);
+
+		cout << "alamat mahasiswa : " << endl;
+		cout << "\t nama Desa : ";
+		cin >> mhs[i].alamat.desa;
+		cout << "\t Nama kota : ";
+		cin >> mhs[i].alamat.kota;
+		cout << "Umur mahasiswa : ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << endl;
+
+}
